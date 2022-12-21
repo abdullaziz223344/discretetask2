9cecfc9ef3049003f922e71c9c99671ca4fc47cb
abdullaziz223344 <120989048+abdullaziz223344@users.noreply.github.com>
2022-12-22 00:05:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static bool checkifperfect(int n)
+{
+    
+    int sum = 1;
+
+   
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            if (i * i != n)
+                sum = sum + i + n / i;
+            else
+                sum = sum + i;
+        }
+    }
+   
+    if (sum == n && n != 1)
+        return true;
+
+    return false;
+}
+
+
+   
+    for (int n = 5; n < 30; n++)
+        if (checkifperfect(n))
+        {
+        
+            Console.WriteLine(n + " is a perfect number");
+        }
+
+
+
